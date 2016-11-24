bf2abfbcb295e85936da03596ea1da2654394862
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2016-11-24 18:03:07 +0000

    Fix minor versions of dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "GeneratedSwiftServer-CloudantStore",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/GeneratedSwiftServer.git", majorVersion: 0)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/GeneratedSwiftServer.git", majorVersion: 0, minor: 1)
     ]
 )
